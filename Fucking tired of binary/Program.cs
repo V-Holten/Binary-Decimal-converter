41a163fdc93a7db323c72d9478c48761b241d429
Victor Heine Holten <vict1856@edu.eal.dk>
2019-03-21 10:16:39 +0100

    Add project files.

---
+++ Fucking tired of binary/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fucking_tired_of_binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calculator calculator = new calculator();
+            string choice;
+            do
+            {
+                Console.WriteLine("Choose:");
+                Console.WriteLine();
+                Console.WriteLine("1. Binary to decimal");
+                Console.WriteLine("2. Decimail to binary");
+                Console.WriteLine("0. Exit");
+
+                choice = Console.ReadLine();
+
+                if (int.TryParse(choice, out int intchoice))
+                {
+                    if (intchoice == 1)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Input: ");
+                        string input = Console.ReadLine();
+                        Console.Clear();
+                        Console.WriteLine(calculator.Binary(input));
+                        Console.WriteLine();
+                    }
+                    else if (intchoice == 2)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Input: ");
+                        string input = Console.ReadLine();
+                        Console.Clear();
+                        Console.WriteLine(calculator.Decimal(input));
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Invalid choice");
+                        Console.WriteLine();
+                    }
+                    
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please choose a number");
+                    Console.WriteLine();
+                }
+                
+            }
+            while (choice != "0");
+        }
+    }
+}
---
+++ Fucking tired of binary/calculator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fucking_tired_of_binary
+{
+    public class calculator
+    {
+        public int Binary(string number)
+        {
+            int result = 0;
+            char[] array = number.ToCharArray();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == '1')
+                {
+                    result += (int)Math.Pow(2.0, i);
+                }
+            }
+            return result;
+        }
+
+        public string Decimal(string number)
+        {
+            string result = "";
+            char[] array = number.ToCharArray();
+            bool notdone = true;
+            int num = int.Parse(number);
+
+            do
+            {
+                if (num % 2 == 1)
+                {
+                    result += "1";
+                }
+                else
+                {
+                    result += "0";
+                }
+                num = num / 2;
+
+                if (num == 0)
+                {
+                    notdone = false;
+                }
+            }
+            while (notdone);
+
+            char[] charArray = result.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
